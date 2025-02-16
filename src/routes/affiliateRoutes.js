15f7a3615df389c89fa5c4f488972159757997c6
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-02-16 10:54:46 -0300

    Update affiliateRoutes.js

--- src/routes/affiliateRoutes.js
+++ src/routes/affiliateRoutes.js
@@ -1,1 +1,13 @@
+// src/routes/affiliateRoutes.js
+const express = require('express');
+const router = express.Router();
+const affiliateController = require('../controllers/affiliateController');
 
+// Rotas
+router.get('/', affiliateController.list);
+router.post('/', affiliateController.create);
+router.get('/:id', affiliateController.getById);
+router.put('/:id', affiliateController.update);
+router.delete('/:id', affiliateController.remove);
+
+module.exports = router;
