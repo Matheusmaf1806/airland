0eca135ce83f4d592f8ac15098cf613bbf1067d1
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-03-15 18:09:18 -0300

    Update supabaseClient.js

--- api/supabaseClient.js
+++ api/supabaseClient.js
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
 
-dotenv.config();
-
+// NÃO precisa de dotenv, pois na Vercel já está definido
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
