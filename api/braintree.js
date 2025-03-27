d5d29128712dbaf9d90fec4fa8a45aaa5a544baa
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-03-27 19:04:37 -0300

    Update braintree.js

--- api/braintree.js
+++ api/braintree.js
@@ -1,12 +1,11 @@
-// braintree.js
-const braintree = require("braintree");
-require("dotenv").config();
+// api/braintree.js
+import braintree from "braintree";
+import dotenv from "dotenv";
+dotenv.config();
 
-const gateway = new braintree.BraintreeGateway({
+export const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
   privateKey: process.env.BRAINTREE_PRIVATE_KEY,
 });
-
-module.exports = { gateway };
