b9e21d5927deada5a131e980e4849d6f9a1a8b06
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-03-30 19:09:28 -0300

    Update malgaClient.js

--- api/malgaClient.js
+++ api/malgaClient.js
@@ -1,6 +1,7 @@
-// api/malgaClient.js
-const axios = require('axios');
-require('dotenv').config();
+// api/malgaClient.js (ESM)
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 const MALGA_API_URL = process.env.MALGA_API_URL;
 const CLIENT_ID = process.env.MALGA_CLIENT_ID;
@@ -9,20 +10,21 @@ const API_KEY = process.env.MALGA_API_KEY;
 const instance = axios.create({
   baseURL: MALGA_API_URL,
   headers: {
-    'Content-Type': 'application/json',
-    'X-Client-Id': CLIENT_ID,
-    'X-Api-Key': API_KEY,
+    "Content-Type": "application/json",
+    "X-Client-Id": CLIENT_ID,
+    "X-Api-Key": API_KEY,
   },
 });
 
-async function createCharge(payload) {
+export async function createCharge(payload) {
   try {
-    const response = await instance.post('/charges', payload);
+    const response = await instance.post("/charges", payload);
     return response.data;
   } catch (error) {
-    console.error('Error creating charge:', error.response ? error.response.data : error.message);
+    console.error(
+      "Error creating charge:",
+      error.response ? error.response.data : error.message
+    );
     throw error;
   }
 }
-
-module.exports = { createCharge };
