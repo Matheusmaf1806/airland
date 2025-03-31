93f922f1c45a343e078c2bcee5aeb559f9fd1839
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-03-30 21:56:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.get("/api/affiliateColors", getAffiliateColors);
 app.use("/api/pay", payRouter); // Rota para integração com PayPal
 
 // ------------------------------------------------------
-// Usar a rota do Malga
+// Usar a rota do Malga (incluindo a nova rota create-transaction)
 app.use("/api/malga", malgaRoutes);
 
 // ------------------------------------------------------
