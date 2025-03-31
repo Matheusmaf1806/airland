22235d6c42ee819a83d069dc12679fd8df22370e
Matheusmaf1806 <matheus.alvarenga@airland.com.br>
2025-03-31 10:52:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
+  // Remova "root: 'src'," para não forçar o Vite a procurar src/index.html
   build: {
-    outDir: '../public',
+    outDir: '../public',   // ou 'public' dependendo de onde está seu vite.config.js
     emptyOutDir: false,
     rollupOptions: {
+      // Define explicitamente o arquivo de entrada, em vez de "index.html"
+      input: 'src/main.js',
       output: {
         entryFileNames: 'assets/main.js'
       }
